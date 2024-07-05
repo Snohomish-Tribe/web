44fd7b7b1a130fe6f5b6f6107dabaf6b1be7e723
DanielekPark <54155014+DanielekPark@users.noreply.github.com>
2024-07-05 16:21:06 -0700

    feat: added nav button feature for mobile devices

---
+++ js/app.js
@@ -0,0 +1,23 @@
+/* =================================
+    Selected Elements
+==================================== */
+const menuBtn = document.querySelector(".toggle");
+const mainNav = document.querySelector(".main-nav");
+const menuIcn = document.querySelector(".toggle img");
+
+let isMenuOpen = false;
+
+/* =================================
+    Event Listeners
+==================================== */
+menuBtn.addEventListener("click", (event) => {
+  isMenuOpen = !isMenuOpen;
+  const btn = event.currentTarget;
+  if (btn.classList.contains("toggle")) {
+    mainNav.classList.toggle("show-links");
+  }
+
+  if (isMenuOpen === true) menuIcn.src = "images/close.svg"; 
+  if (isMenuOpen === false) menuIcn.src = "images/hamburger.svg"; 
+  
+});
