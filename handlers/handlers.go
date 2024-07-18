d9d00f1744b28ded27b2b39dc22ccf86f792db60
DanielekPark <54155014+DanielekPark@users.noreply.github.com>
2024-07-18 09:56:32 -0700

    feat: added handlers and connected css styles to html

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -8,6 +8,15 @@ import (
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	tmpl, _ := template.ParseFiles("templates/index.html")
+	http.FileServer(http.Dir("static"))
+	err := tmpl.Execute(w, nil)
+
+	if err != nil {
+		fmt.Println("Error")
+	}
+}
+func About(w http.ResponseWriter, r *http.Request) {
+	tmpl, _ := template.ParseFiles("templates/about.html")
 
 	err := tmpl.Execute(w, nil)
 
--- main.go
+++ main.go
@@ -7,8 +7,10 @@ import (
 )
 
 func main() {
-
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
 
 	http.ListenAndServe(":3000", nil)
 }
