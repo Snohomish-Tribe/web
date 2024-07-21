6e5bdde44b26ede29adb00dcc1c896607c5da45b
DanielekPark <54155014+DanielekPark@users.noreply.github.com>
2024-07-20 17:33:28 -0700

    feat: added a route for the contact page

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/danielekpark
 
 go 1.22.1
+
+require (
+	github.com/cwinters8/gomap v0.1.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
--- main.go
+++ main.go
@@ -10,6 +10,7 @@ func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
+	http.HandleFunc("/contact", handlers.Contact)
 
 	http.ListenAndServe(":3000", nil)
 }
