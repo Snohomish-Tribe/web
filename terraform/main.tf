013a83aff976b3b2baf8c14951cd188646294b8d
Clark Winters <github@clarkwinters.com>
2024-08-13 21:29:54 -0500

    add s3 compatible backend for terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,21 @@ terraform {
       source = "oracle/oci"
     }
   }
+
+  backend "s3" {
+    bucket                      = "terraform"
+    region                      = "us-sanjose-1"
+    key                         = "terraform.tfstate"
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    use_path_style              = true
+    skip_s3_checksum            = true
+    skip_metadata_api_check     = true
+    endpoints = {
+      s3 = "https://axihvv9biq8w.compat.objectstorage.us-sanjose-1.oraclecloud.com"
+    }
+  }
 }
 
 provider "oci" {
