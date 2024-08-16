097e35a0a4a3b31c6603ec92b0652877fce2e7de
Clark Winters <github@clarkwinters.com>
2024-08-16 10:05:03 -0500

    WIP: subnet and container instance

--- terraform/container.tf
+++ terraform/container.tf
@@ -6,5 +6,31 @@ resource "oci_artifacts_container_repository" "web" {
 }
 
 output "container_repo_image_tag" {
-  value = "ocir.${var.region}.oci.oraclecloud.com/${oci_artifacts_container_repository.web.namespace}/${oci_artifacts_container_repository.web.display_name}"
+  value = local.container_image_tag
+}
+
+locals {
+  container_image_tag = "ocir.${var.region}.oci.oraclecloud.com/${oci_artifacts_container_repository.web.namespace}/${oci_artifacts_container_repository.web.display_name}"
+}
+
+resource "oci_container_instances_container_instance" "web" {
+  availability_domain = "${var.region}-ad-1"
+  compartment_id      = var.compartment_id
+  containers {
+    image_url = "${container_image_tag}:latest"
+  }
+  shape = "CI.Standard.A1.Flex"
+  shape_config {
+    ocpus         = 1
+    memory_in_gbs = 5
+  }
+  vnics {
+    subnet_id = oci_core_subnet.web.id
+  }
+}
+
+resource "oci_core_subnet" "web" {
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.internal.id
+  cidr_block     = "172.16.1.0/24"
 }
