b007640f40370a806f4ca83be6598fce10052070
DanielekPark <54155014+DanielekPark@users.noreply.github.com>
2024-08-24 18:51:23 -0700

    updated javascript animation for timeline

--- js/timeline.js
+++ js/timeline.js
@@ -3,22 +3,54 @@
 ==================================== */
 const btn = document.querySelector(".btn");
 const aside = document.querySelector("aside"); 
-const col = document.querySelector(".row-3 .col-2")
+const col = document.querySelector(".row-3 .col-2");
 
 let hideTimeline = true;
 
 /* ================================= 
     Event Listeners
 ==================================== */
+/*  
 btn.addEventListener("click", () => {
-    hideTimeline = !hideTimeline;    
+    hideTimeline = !hideTimeline;
+
     if (hideTimeline === false) {
-        btn.textContent = "Collapse"; 
+        btn.textContent = 'Collapse';
+        col.style.height = `${aside.offsetHeight}px`;
+        col.classList.add('expand')
     } 
-
+    
     if (hideTimeline === true) {
-        btn.textContent = "Expand";
+        btn.textContent = 'Expand';
+        col.classList.remove('expand')
+        col.style.height = '450px'
     }
+});*/
 
-    col.classList.toggle("expand")
+btn.addEventListener("click", () => {
+    hideTimeline = !hideTimeline;
+
+    if (hideTimeline === false) {
+        btn.textContent = 'Collapse';
+        col.classList.add('expand')
+    } 
+    
+    if (hideTimeline === true) {
+        btn.textContent = 'Expand';
+        col.classList.remove('expand')
+        col.classList.remove('full')
+    }
 });
+
+col.addEventListener('transitionend', () => {
+    if(col.classList.contains('expand')) {
+        col.classList.add('full')
+    }
+})
+
+
+// window.addEventListener('resize', () => {
+//     if(col.classList.contains('expand')) {
+//         col.style.height = `${aside.offsetHeight}px`;
+//     }
+// })
