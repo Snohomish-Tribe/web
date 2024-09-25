23ffd18dbe7d8b5065d999eddbda5fd4f4d7dd6c
DanielekPark <54155014+DanielekPark@users.noreply.github.com>
2024-09-25 13:19:27 -0700

    Added Javascript to dynamically show current in the footer

--- static/js/app.js
+++ static/js/app.js
@@ -5,8 +5,12 @@ const menuBtn = document.querySelector(".toggle");
 const mainNav = document.querySelector(".main-nav");
 const closeIcn = document.querySelector(".close");
 const menuIcn = document.querySelector(".menu");
+const year = document.querySelector('.year');
 
 let isMenuOpen = false;
+
+//Displays year in the footer
+year.textContent = year.textContent = `@ ${new Date().getFullYear()} Snohomish Tribe of Indians`;
 
 /* =================================
     Event Listeners
