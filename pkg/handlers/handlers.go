0a0f19f4ab203f98de7ef9d6be1612991554265e
DanielekPark <54155014+DanielekPark@users.noreply.github.com>
2025-12-02 13:42:07 -0800

    Added strapi cms for events page

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -4,11 +4,15 @@ import (
 	"fmt"
 	"html/template"
 
+	"encoding/json"
+	"io"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/cwinters8/gomap"
+	"github.com/joho/godotenv"
 	"github.com/snohomishtribe/pkg/models"
 )
 
@@ -25,6 +29,37 @@ func checkPath(expectedPath string, w http.ResponseWriter, r *http.Request) erro
 		}
 	}
 	return nil
+}
+
+func FetchEvents(url string, token string) models.EventsPageData {
+	godotenv.Load()
+	var strapiRes models.EventsPageData
+
+	client := &http.Client{Timeout: time.Second * 10}
+
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Add("Accept", "*/*")
+	req.Header.Add("Authorization", "Bearer "+token)
+
+	res, err := client.Do(req)
+	if err != nil {
+		defer func() {
+			if r := recover(); r != nil {
+				fmt.Println("There was no response from Strapi")
+			}
+		}()
+	}
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println("Unable to read response")
+	} else {
+		json.Unmarshal(body, &strapiRes)
+		fmt.Println(strapiRes)
+	}
+
+	defer res.Body.Close()
+
+	return strapiRes
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
@@ -138,13 +173,21 @@ func Credits(w http.ResponseWriter, r *http.Request) {
 }
 
 func Events(w http.ResponseWriter, r *http.Request) {
+	token := os.Getenv("PROD_EVENTS_BEARER_TOKEN")
+	url := os.Getenv("PROD_EVENTS_ENDPOINT")
+	ParsedData := FetchEvents(url, token)
+
 	if err := checkPath("/events", w, r); err != nil {
 		log.Fatal(err)
 	}
 	tmpl, _ := template.ParseFiles("static/templates/events.html", "static/templates/main.layout.html")
 
-	if err := tmpl.Execute(w, nil); err != nil {
-		log.Fatal("Failed to parse template ", err)
+	// if err := tmpl.Execute(w, nil); err != nil {
+	// 	log.Fatal("Failed to parse template ", err)
+	// }
+
+	if err := tmpl.Execute(w, ParsedData); err != nil {
+		log.Fatal("Failed to parse template", err)
 	}
 }
 
--- pkg/models/reqbody.go
+++ pkg/models/reqbody.go
@@ -6,3 +6,15 @@ type Reqbody struct {
 	Question string `json:"questions"`
 	Message  string `json:"message"`
 }
+
+type EventsPageData struct {
+	Data []struct {
+		Day        string `json:"Day"`
+		Date       string `json:"Date"`
+		Month_Year string `json:"Month_Year"`
+		Time       string `json:"Time"`
+		Event_Name string `json:"Event_Name"`
+		Address    string `json:"Address"`
+		Details    string `json:"Details"`
+	} `json:"data"`
+}
